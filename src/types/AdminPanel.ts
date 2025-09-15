1e3cc9aecc7281148793bf65441951a438ba2f2a
SanjoliGoyal1262 <sanjoli1262.be21@chitkara.edu.in>
2025-09-15 19:15:45 +0530

    [Sanjoli] Add: Adding routing page

---
+++ src/types/AdminPanel.ts
@@ -0,0 +1,23 @@
+export interface Movie {
+  id?: number;
+  title: string;
+  genre: string;
+  rating: number | string;
+  poster_url: string | File;
+  banner_url: string | File;
+  release_year: number;
+  director: string;
+  description: string;
+  duration: number | string;
+  premium:boolean
+}
+
+export const genres: string[] = [
+  "Action",
+  "Romance",
+  "Thriller",
+  "Drama",
+  "Comedy",
+  "Sci-Fi",
+  "Horror",
+];
---
+++ src/types/MainMovieCard.ts
@@ -0,0 +1,19 @@
+export interface Movie {
+  id: number;
+  title: string;
+  banner_url?: string;
+  poster_url?: string;
+  rating: number;
+  release_year: number;
+  director: string;
+  premium: boolean;
+  duration:number
+}
+
+export interface MovieCardProps {
+  movie: Movie;
+  userRole: string;
+  onCardClick: (movieId: number,premium:boolean) => void;
+  onEditClick: (movie: Movie) => void;
+  onDeleteClick: (movieId: number) => void;
+}
